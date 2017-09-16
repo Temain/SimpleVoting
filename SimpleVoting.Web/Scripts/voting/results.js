77e097b8a73f6999ff88c1be0bef7687256d33fe
temain <asv.artm@gmail.com>
2017-09-16 21:05:24 +0300

    Cleaning

---
+++ SimpleVoting.Web/Scripts/voting/results.js
@@ -0,0 +1,27 @@
+﻿function ResultsViewModel() {
+    var self = this;
+    self.Questions = ko.observableArray([]);
+
+    self.InitSemantic = function () {
+        $('.ui.progress').progress();
+    };
+
+    self.LoadData = function () {
+        $.ajax({
+            url: '/api/voting/results',
+            type: 'GET',
+            success: function (response) {
+                self.Questions(response);
+                self.InitSemantic();
+            },
+            error: function (error) {
+                var message = 'При получении данных произошла ошибка';
+                console.log(message);
+                var noty = new Noty(notyOptions);
+                noty.options.type = 'error';
+                noty.options.text = message;
+                noty.show();
+            }
+        });
+    };
+}
